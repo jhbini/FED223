ff51a3a0be6d63a3a20e367fdd0d46204c0d7e78
jhbini <hebin777@naver.com>
2022-08-02 17:21:08 +0900

    1

--- 001.HTML학습/08.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/08.VOGUE_PJ/002.site/js/common.js
@@ -41,7 +41,7 @@ $(() => {
     }); //////////// each ///////////////
 
     // 위치 배열값 확인
-    console.log(scpos);
+    // console.log(scpos);
     //    scpos.forEach((val)=>console.log(val));
 
     /************************************************************* 
@@ -81,12 +81,12 @@ $(() => {
         });
 
         // 위치 배열값 확인
-        console.log(scpos);
+        // console.log(scpos);
 
     }); /////////////// resize 함수 ////////////
 
-        // 부드러운 스크롤 위치변수 pos 업데이트
-        // pos = scTop
+    // 부드러운 스크롤 위치변수 pos 업데이트
+    // pos = scTop
 
     /////////////////////////////////////////
     //// 윈도우에 스크롤 이벤트 설정하기 ////
@@ -163,5 +163,95 @@ $(() => {
 
     }); /////// click ///////
 
+    // 로딩테스트용 동영상 넣기
+    // $("body").prepend('<iframe width="640" height="360" src="https://www.youtube.com/embed/FLNUcZxCT6k" title="[한산: 용의 출현] 티저 예고편" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 
 }); ////////////////// jQB //////////////////
+
+
+// 윈도우 객체 컨텐츠가 모두 로딩된 후 로딩박스 없애기
+$(window).on("load", function () {
+    $(".LDbx").hide();
+}); ////////////// load ///////////////
+
+/****************************************************************** 
+    제이쿼리 스크롤 멈춤 체크 플러그인 메서드
+******************************************************************/
+$.fn.scStop = function (callback) {
+
+    // 스크롤 이벤트 설정하기//
+    $(this).scroll(function () {
+        // 1. 먼저 타임아웃지우기(아래서 설정된 타임아읏)
+        clearTimeout($(this).data('scTime'));
+        // 2. 타임아웃 설정을 한다.
+        $(this).data('scTime', setTimeout(callback, 80))
+        // 제이쿼리 data() 메서드
+        // -> data(키,값) 특정이름에 값을 셋팅하여 저장
+        // 타임아웃에 시간은 부드러운 스크롤에 셋팅된 시간인
+        // speed, smooth 시간을 더한 값으로 넣어준다.
+        // 예) new SmoothScroll(document, 60, 20)-> 60+20=80
+        // 80dms 1/1000초 (0.08초)
+
+    }); //////// 스크롤시 계속 발생구역 //////
+
+}; /////////////// 스크롤멈춤 플러그인 /////////////
+
+//// 윈도우객체를 제이쿼리로 플러그인 호출
+// 이때 실행될 함수를 전달함
+$(window).scStop(function () {
+    // 마지막 스크롤 위치값
+    let scTop = $(this).scrollTop();
+    // 콘솔에 확인
+    // console.log("스크롤멈춤", scTop);
+    // 부드러운 스크롤 위치값에 업데이트
+    pos = scTop;
+}); ///////// window /////////////////
+
+$(() => {
+
+    // 제이쿼리 data(키,값) 연습
+    let mykey = $("#top");
+    mykey.data('헐', '고양이');
+    mykey.data('힘', '강아지');
+
+    // console.log('data()키값:',
+    //     mykey.data('헐'), '/', mykey.data('힘'));
+
+    // data() 삭제는 removeData(키)
+    mykey.removeData('헐');
+
+    // console.log('data()키값:',
+    //     mykey.data('헐'), '/', mykey.data('힘'));
+
+}); ////////////// jQB ////////
+
+///////////////////// 비교 JS Map() 메서드 /////////////////////
+// -> 키,값으로 저장하는 메모리공간 생성 메서드 ///////////
+// 1. 메모리 공간 생성: Map() -> new 키워드로!
+// Map() 객체 아레 메서드 :set(), get(), has(), delete()
+const keyval = new Map();
+
+// 2. 값셋팅: set(키,값)
+keyval.set('한국배우','조승우');
+keyval.set('미국배우','탐크루즈');
+keyval.set('중국배우','성룡');
+
+// 3. 값호출 : get(키)
+// console.log('Map()값:',
+// keyval.get('한국배우'),'/',
+// keyval.get('미국배우'),'/',
+// keyval.get('중국배우')
+// )
+// 4. 키존재여부: has(키)
+// console.log('성룡있나? 지금?',keyval.has('중국배우'));
+
+// 5. 값 삭제: delete(키)
+keyval.delete('중국배우');
+
+// console.log('성룡있나? 지금?',keyval.has('중국배우'));
+
+// console.log('Map()값:',
+// keyval.get('한국배우'),'/',
+// keyval.get('미국배우'),'/',
+// keyval.get('중국배우')
+// )
--- 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 004.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -1,1 +1,435 @@
 // 제이쿼리 기본 JS - main.js 
+
+// 제이쿼리 코드블록
+$(() => {
+
+    // 0. 주인공들 변수할당하기!
+    // (1) 미니언즈
+    let mi = $(".mi");
+
+    // (2) 건물 li
+    let bd = $(".building li");
+
+    // (3) 버튼들
+    let btns = $(".btns button");
+
+    // (4) 메시지 박스
+    let msg = $(".msg");
+
+    // 1. 건물에 각방 번호 넣기
+    bd.each((idx, ele) => { // idx-순번, ele-요소
+        // text() 텍스트 넣기
+        $(ele).text(idx);
+        // 9번방에 좀비넣기
+        if (idx === 9)
+            $(ele).append('<img src="images/mz1.png"alt="좀비1" class="mz">');
+        else if (idx === 7)
+            $(ele).append('<img src="images/mz2.png"alt="좀비2" class="mz">');
+        else if (idx === 1)
+            $(ele).append('<img src="images/zom.png"alt="좀비들" class="mz">');
+        else if (idx === 2)
+            $(ele).append('<img src="images/inj.png"alt="좀비들" class="inj">');
+    }); ///////// each ///////////////
+
+    // 좀비는 모두 숨기기: hide()
+    $(".mz").hide();
+
+    // 2. 버튼 셋팅하기
+    // 모든 버튼은 숨기고 첫번째 버튼만 보여
+    // 버튼.숨겨().첫번째().보여()
+    btns.hide().first().show();
+    // btns.hide().last().show();
+
+    // 3. 공통 구현 함수 만들기
+    // 각 스텝에서 미닐언즈가 할 공통 기능 함수구현
+    const actMini = (ele, seq, fn) => {
+        // ele - 버튼자신
+        // seq - 이동할 li 순번
+        // fn - 이동 후 실행함수
+
+        // 0. 버튼자신 없애기
+        // 없애기 방법 : hide(),slideUp(),fadeOut()
+        $(ele).slideUp(300);
+        // + 메시지 없애기
+        msg.hide();
+
+        // 1. 위치값 알아오기(대상:빌딩li)
+        // 8번째 li
+        // eq(순번) -> 순번요소 선택 메서드!
+        let tg = bd.eq(seq); // 타켓 li
+        let tval = bd.eq(seq).offset().top;
+        let lval =
+            tg.offset().left + tg.width() / 2 - mi.width() / 2;
+        // left위치 보정으로 미니언즈 li의 중앙위치
+        // 계산: left위치 + li가로 절반 - 미니언즈 가로절반
+
+        // offset() 메서드 - 요소셋팅값(위치,크기)
+        // top,left,width,height 속성값 사용!
+        // width(),height() 메서드로 바로 사로, 세로크기 구함
+
+
+        // 2. 위치이동(이동대상:미니언즈)
+        mi.animate({
+            top: tval + "px",
+            left: lval + "px"
+        }, 1000, "easeInOutElastic", fn);
+        ////////// animate //////////
+
+    }; /////////// actMini 함수 /////////////////
+    /////////////////////////////////////////////
+
+    // 4. "들어가기"버튼 클릭시
+    btns.first().click(function () {
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 메시지 변경하기+보이기
+                msg.text("와~ 아늑하다 옆방으로 가보자!")
+                    .show();
+
+                //다음버튼 보이기
+                $(this).next().fadeIn(300);
+                // console.log(this);
+
+                // this키워드는 누구인가?
+                // function(){} 일반 익명 함수에서는
+                // 가장 가깝게 싸고 있는 대상이 주인공이다.
+                // mi의 콜백함수로 실행하므로
+                // this는 mi가됨
+                // 그러나....
+                // 화살표함수 ()=>{}는 this키워드의 주인공은
+                // 최상위 이벤트 주인공인 버튼 자신이 된다.
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 8, fn);
+
+        }) ///////// 들어가기 click ///////////
+
+        // 5. "옆방으로"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 좀비 나타나기(2초후)
+                bd.eq(9).find(".mz")
+                    .delay(2000).fadeIn(400,
+                        () => { // 좀비나온 후
+
+                            // 메시지
+                            msg.html(`
+                        악!! 좀비! <br>
+                        어서피하자!
+                    `)
+                                .css({
+                                    left: "-154%"
+                                }) // 메시지위치
+                                .fadeIn(200); //보이기
+
+                            //다음버튼 보이기
+                            $(this).next().fadeIn(300);
+
+                        }); /////////// fadeIn /////////////
+
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 9, fn);
+
+        }) ///////////// "옆방으로" click ///////////////
+
+        // 6. "윗층으로 도망가"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 메시지 보이기
+                msg.html(`여긴없겠지?`)
+                    .fadeIn(200);
+
+                // 좀비보이기
+                bd.eq(7).find(".mz")
+                    .delay(1000).fadeIn(400, () => {
+
+                        // 메시지 변경하기
+                        msg.html(`악! 여기도!!!`);
+
+                        //다음버튼 보이기
+                        $(this).next().fadeIn(300);
+                    }); //////////// fadeIn ////////////
+
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 7, fn);
+
+        }) ///////////// "윗층으로 도망가" click ///////////////
+
+        // 6. "다시옆방으로!"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 첫번째 메시지
+                msg.html('여긴없겠지?')
+                    .fadeIn(200)
+                    .delay(1000) // 지연시간은 애니메이션 앞에서 효과
+                    .fadeIn(200, // 두번째 메시지
+                        () => {
+                            msg.html('그래도 무서우니<br> 윗층으로 가자')
+                            //다음버튼 보이기
+                            $(this).next().fadeIn(300);
+
+                        }); ///////////// fadeIn ////////////
+
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 6, fn);
+
+        }) ///////////// "다시옆방으로!" click ///////////////
+
+        // 7. "무서우니 윗층으로!"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 무.서.워... 메시지
+                msg.text('무')
+                    .fadeIn(200)
+                    .delay(500)
+                    .fadeIn(200, () => msg.text('무.'))
+                    .delay(500)
+                    .fadeIn(200, () => msg.text('무.서'))
+                    .delay(500)
+                    .fadeIn(200, () => msg.text('무.서.'))
+                    .delay(500)
+                    .fadeIn(200, () => msg.text('무.서.워'))
+                    .delay(500)
+                    .fadeIn(200, () => msg.text('무.서.워.'))
+                    .delay(500)
+                    .fadeIn(200, () => msg.text('무.서.워..'))
+                    .delay(500)
+                    .fadeIn(200, () => msg.text('무.서.워...'))
+                    .delay(500)
+                    .fadeIn(200,
+                        () => {
+                            // 7번방 좀비가 올라와서 달려든다
+                            bd.eq(7).find(".mz")
+                                .animate({ // 윗층으로 올라옴
+                                    bottom: bd.eq(7).height() + "px"
+                                }, 200, "easeOutElastic")
+                                .delay(500)
+                                .animate({ // 달려들기
+                                        right: (bd.eq(7).width() * 1.1) + "px"
+                                    }, 1000, "easeInExpo",
+                                    () => { // 물린후 대사
+                                        msg
+                                            .css({
+                                                left: "-110%"
+                                            })
+                                            .html('아~악 물렸다 <br> 어서 치료주사방으로!')
+                                        // 미니언즈 좀비 이미지 변경(1초후)
+                                        setTimeout(() => {
+                                            mi.find("img")
+                                                .attr("src", "images/mz1.png");
+                                        }, 1000);
+
+                                        //다음버튼 보이기
+                                        $(this).next().fadeIn(300);
+
+                                    }); /////////// animate //////////
+                        }); ///////////////// fadeIn ////////////
+
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 4, fn);
+
+        }) ///////////// "무서우니 윗층으로!" click ///////////////
+
+        // 8. "치료주사방으로!"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 주사기 회전
+                $('.inj').css({
+                    transform: "rotate(-180deg)",
+                    transition: ".5s .5s",
+                    zIndex: 999999999
+                }); //////////// css ///////////
+
+                // 미니언즈로 다시태어나다!(1초후)
+                setTimeout(() => {
+                    // 이미지변경
+                    mi.find("img").attr("src", "images/m2.png");
+                    // 대사
+                    msg.html("이제 조금만 더 <br> 가면 탈출이다!")
+                        .css({
+                            left: "-150%"
+                        })
+                        .fadeIn(200)
+                }, 1000);
+                //다음버튼 보이기
+                $(this).next().fadeIn(300);
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 2, fn);
+
+        }) ///////////// "치료주사방으로!" click ///////////////
+
+        // 9. "3번방으로"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 메시지 보이기
+                msg.html("어서 윗층으로 가자")
+                    .fadeIn(200);
+
+                //다음버튼 보이기
+                $(this).next().fadeIn(300);
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 3, fn);
+
+        }) ///////////// "3번방으로" click ///////////////
+
+        // 10. "1번방으로"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+            // 이동후 함수
+            let fn = () => {
+
+
+                // 메시지 보이기
+                msg.html("이제 곧 탈출이다!")
+                    .fadeIn(200);
+
+                //다음버튼 보이기
+                $(this).next().fadeIn(300);
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 1, fn);
+
+        }) ///////////// "1번방으로" click ///////////////
+
+        // 11. "헬기를 호출"버튼 클릭시 ///////
+        .next().click(function () {
+
+
+            // 이동후 함수
+            let fn = () => {
+
+                // 메시지 보이기
+                msg.html("도와줘요!")
+                    .fadeIn(200);
+
+                // 1번 좀비들이 나타나서 달려듬
+                bd.eq(1).find(".mz")
+                    .fadeIn(300)
+                    .animate({
+                        right: bd.eq(1).width() + "px"
+                    }, 3000, "easeInExpo");
+
+                // 헬기등장하기
+                $(".heli").animate({
+                            left: "20%"
+                        }, 4000, "easeOutBack",
+
+                        function () { // 헬기도착 후 탑승이미지 변경
+                            $(this)
+                                .attr("src", "images/heli2.png");
+                            // 미니언즈 없애기
+                            mi.hide();
+                        })
+                    .delay(500) // 0.5초 쉬었다가
+                    .animate({ // 다시 오른쪽 끝으로 이동
+                            left: "70%"
+                        }, 4000, "easeInOutCirc",
+                        function () { // 끝쪽에서 조정사 좀비로!
+                            $(this)
+                                .attr("src", "images/heli3.png");
+                        })
+                    .delay(300)
+                    .animate({ // 아주 천천히 나감!
+                            left: "100%"
+                        }, 1000, "linear",
+                        () => {
+                            // 최종 마무리 구역 //
+
+                            // 간판 떨어뜨리기
+                            // 1단계 : 중간까지 떨어짐
+                            // -> 간판에 class "on"주기
+                            let tit = $(".tit");
+                            tit.addClass("on");
+                            setTimeout( // 3초후 2단계 다떨어짐
+                                // -> 간판에 class "on2"추가
+                                () => tit.addClass("on2")
+                                ,3000);
+
+                            // 빌딩 무너뜨리기
+                            // 간판 떨어진 후 실행(6초후)
+                            setTimeout(() =>
+                                bd.parent().addClass("on")
+                                // parent() 부모요소인 .building
+                                , 6000);
+                        }) ////////// animate //////////
+
+                //다음버튼 보이기
+                $(this).next().fadeIn(300);
+
+            }; ////////////// fn /////////////
+
+            // 공통 기능구현 함수 호출
+            actMini(this, 0, fn);
+
+        }) ///////////// "헬기를 호출" click ///////////////
+
+    // 간판에 마우스 오버시 / 아웃시 색상변경
+    // hover(함수1,함수1)
+    // -함수1은 오버시, 함수2는 아웃시 실행
+    $(".tit").hover(
+        function () { // over
+            $(this).css({
+                backgroundColor: "blue",
+                color: "pink",
+                transition: "none"
+            });
+        },
+        function () { // out
+            $(this).css({
+                backgroundColor: "pink",
+                color: "yellow"
+            });
+        }); ////////////// hover ///////////
+
+
+}); /////////////////////// jQB ///////////////////
